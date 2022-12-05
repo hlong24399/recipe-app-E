5ab8af44e97a2da2c144e98ab9692a32047a186a
Hoang Long <hlong24399@gmail.com>
2022-12-05 15:22:03 -0800

    starting point

---
+++ recipe-data.js
@@ -0,0 +1,19 @@
+const recipes = [
+    {
+        name : "Shepard's Pie",
+        img : "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/12/Shepherds-Pie-8.jpg",
+        description: "Dish of British Isles origin that makes use ofvegetables and chopped meat cooked in a casserole topped with or surrounded by mashed potatos.",
+    },
+    {
+        name : "Chicken & Dumplings",
+        img : "https://thecozycook.com/wp-content/uploads/2021/12/Chicken-and-Dumplings-f-2.jpg",
+        description: "This dish is a soup which involves chicken being cooked in water, with the left-over chicken broth used to cook the dumplings by boiling them.",
+    },
+    {
+        name : "Reuban Sandwhich",
+        img : "https://iambaker.net/wp-content/uploads/2019/08/reuben-1.jpg",
+        description: "Sandwhich consisting of corned beef, swiss chees, saurkraut, Russian dressing between slices of rye bread which is grilled to the point where the bread is crispy with the cheese melting.",
+    },
+]
+
+module.exports = recipes;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { engine } = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000
+const recipe_data = require('./recipe-data');
+
+// enable middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// enable public folder as static contents
+app.use(express.static(path.join(__dirname, "public")));
+
+// default
+app.get('/', (req, res) => {
+  res.send(recipe_data);
+})
+
+app.get('/home', (req, res) => {
+    res.render('home');
+})
+
+app.get('/recipe/:id', (req, res) => {
+    id = req.params.id;
+    res.render('recipe', {recipe: recipe_data[id]} );
+})
+
+app.get('/recipe', (req, res) => {
+    id = req.params.id;
+    res.render('recipe', {recipe: recipe_data[0]} );
+})
+
+
+// select handlebars as engine for this view
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.set('views', './views');
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
